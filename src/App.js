df58c3af303d105d56b28e40f2220f483b345488
eddiebee <edbeeotudor@gmail.com>
2022-05-13 12:55:30 +0100

    Initial app setup and build kickstart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={logo}
+          alt="Redux logo"
+          width={100}
+          height={100}
+          className="App-logo"
+        />
+        <h2>Redux Implementations</h2>
       </header>
+      <main>
+        <h5>...this app will enable you add and subtract (many more)</h5>
+      </main>
     </div>
   );
 }
